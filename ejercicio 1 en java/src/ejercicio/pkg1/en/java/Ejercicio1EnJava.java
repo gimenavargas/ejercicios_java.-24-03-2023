3d4459be30969fe5b0d6b0384b958bb7326284c4
gimenavargas <1471366@senati.pe>
2023-03-24 09:28:15 -0500

    grafico de letras

--- ejercicio 1 en java/src/ejercicio/pkg1/en/java/Ejercicio1EnJava.java
+++ ejercicio 1 en java/src/ejercicio/pkg1/en/java/Ejercicio1EnJava.java
@@ -17,7 +17,7 @@ public class Ejercicio1EnJava {
         System.out.println("     J   a   V     V  a    ");
         System.out.println("     J  a a   V   V  a a   ");
         System.out.println("  J  J aaaaa   V V  aaaaa  ");
-        System.out.println("   JJ  a   a    V  a   a   ");
+        System.out.println("   JJ  a   a    V  a     a ");
 
 
     }
