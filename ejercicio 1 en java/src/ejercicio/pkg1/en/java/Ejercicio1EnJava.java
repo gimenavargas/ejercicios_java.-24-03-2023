0d296e32c8b8a7be4fe0783a8f493edb78d2bbee
gimenavargas <1471366@senati.pe>
2023-03-24 11:16:46 -0500

    ingreso de 2 numeros

--- ejercicio 1 en java/src/ejercicio/pkg1/en/java/Ejercicio1EnJava.java
+++ ejercicio 1 en java/src/ejercicio/pkg1/en/java/Ejercicio1EnJava.java
@@ -14,10 +14,10 @@ public class Ejercicio1EnJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("     J   a   V     V  a    ");
-        System.out.println("     J  a a   V   V  a a   ");
-        System.out.println("  J  J aaaaa   V V  aaaaa  ");
-        System.out.println("   JJ  a   a    V  a     a ");
+        System.out.println("     J    a   V     V  a    "); 
+        System.out.println("     J   a a   V   V  a a   ");
+        System.out.println("   J J  aaaaa   V V  aaaaa  ");
+        System.out.println("    JJ a     a    V  a     a ");
 
 
     }
---
+++ ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio2;
+import java.util.Scanner;
+/**
+ *
+ * @author Alumno
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // 
+        int n;
+        int y;
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Escribe el primer numero ");
+        n=leer.nextInt (); //n recoge el primer valor escrito por teclado
+        System.out.print("Escribe el segundo numero ");
+        y=leer.nextInt (); //y recoge el segundo valor escrito por teclado
+        
+        if (n != y) //condicion si n es diferente que y
+        {
+            System.out.println("el numero "+n+" es diferente a "+y);
+        }
+        if (n < y) //condicion si n es menor que y
+        {
+            System.out.println("el numero "+n+" es menor que "+y);
+        }
+        if (n > y) //condicion si n es mayor que y
+        {
+            System.out.println("el numero "+n+" es mayor que "+y);
+        }
+        if(n <= y) //condicion si n es mayor o igual que y
+        {
+            System.out.println("el numero "+n+" es menor o igual que "+y);
+          
+        }
+    }
+    
+    
+}
