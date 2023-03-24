ddb6296b8d0ee18d0b7b52db4b34c785fc79fe51
gimenavargas <1471366@senati.pe>
2023-03-24 09:26:00 -0500

    Programa de java que muestra un grafico

---
+++ ejercicio 1 en java/src/ejercicio/pkg1/en/java/Ejercicio1EnJava.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio.pkg1.en.java;
+import java.util.Scanner;
+/**
+ *
+ * @author Alumno
+ */
+public class Ejercicio1EnJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("     J   a   V     V  a    ");
+        System.out.println("     J  a a   V   V  a a   ");
+        System.out.println("  J  J aaaaa   V V  aaaaa  ");
+        System.out.println("   JJ  a   a    V  a   a   ");
+
+
+    }
+    
+}
